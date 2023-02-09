638f866fe6169cb1a88e3564cff4d7ea60b46fd4
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-02-09 21:41:59 +0000

    Added Refresh Token logic

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -9,13 +9,14 @@ export const load = (async ({ cookies, url }: {cookies:Cookies, url:URL}) => {
 	// if redirected from o-auth code will be in params
 	if (url.searchParams.has("code")) {
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		const authStatus = await stravaAuthenticate(url.searchParams.get("code")!, SECRET_clientSecret)
+		const authStatus = await stravaAuthenticate(url.searchParams.get("code")!, SECRET_clientSecret, 'authorization_code')
 		console.log(authStatus)
 		if (authStatus) {
 			// Front end can create an API object
-			const { access_token, refresh_token } = authStatus
+			const { access_token, refresh_token, expires_at } = authStatus
 			cookies.set("access_token", access_token)
 			cookies.set("refresh_token", refresh_token)
+			cookies.set("expires_at", String(expires_at))
 
 			return {
 				access_token, refresh_token, status: true, message: null
@@ -31,8 +32,30 @@ export const load = (async ({ cookies, url }: {cookies:Cookies, url:URL}) => {
 	// try and get data from cookie
 	const access_token = cookies.get("access_token")
 	if (access_token) {
-		const refresh_token = cookies.get("refresh_token")
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const refresh_token = cookies.get("refresh_token")!
 
+		if (Number(cookies.get("expires_at")) < Date.now()) {
+			const authStatus = await stravaAuthenticate(refresh_token, SECRET_clientSecret, 'refresh_token')
+			console.log(authStatus)
+			if (authStatus) {
+				// Front end can create an API object
+				const { access_token, refresh_token, expires_at } = authStatus
+				cookies.set("access_token", access_token)
+				cookies.set("refresh_token", refresh_token)
+				cookies.set("expires_at", String(expires_at))
+
+				return {
+					access_token, refresh_token, status: true, message: null
+				}
+			}
+
+			// Notify User Authentication failed
+			return {
+				status: false,
+				message: "Refresh of Strava Authentication failed"
+			}
+		}
 		return {access_token, refresh_token, status: true}
 	}
 	

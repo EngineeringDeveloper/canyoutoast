42c8ee85a225fb77b7c958fd892c2918ec2194fb
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-02-10 22:50:36 +0000

    Start of a css toaster

---
+++ src/lib/toaster.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    // https://www.youtube.com/watch?v=G0HtHr46Awg&ab_channel=CodingArtist
+</script>
+<div class="m-auto w-fit h-fit absolute inset-0">
+    <div class="relative top-0 left-0 bg-blue-600 w-96 h-72 rounded-t-3xl"></div>
+    <div class="absolute top-0 right-0 bg-blue-300 w-16 h-72 rounded-t-3xl"></div>
+</div>
+<!-- <slot /> -->
+
+<style>
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import type {binLength as BinLength} from "$lib/types"
+	import type { binLength as BinLength } from '$lib/types';
 	import { Strava, stravaOAuth } from './api';
 	import { SyncLoader } from 'svelte-loading-spinners';
 
 	import type { PageData } from './$types';
 	import Toast from '../lib/toast.svelte';
+	import Toaster from '../lib/toaster.svelte';
 
 	export let data: PageData;
 
@@ -13,11 +14,11 @@
 		api = new Strava(data.access_token, data.refresh_token);
 	}
 
-    const bins: BinLength = [300, 400, 500, 600, 700, 800, 900, 1000, 1200, 1300, 1400]
+	const bins: BinLength = [300, 400, 500, 600, 700, 800, 900, 1000, 1200, 1300, 1400];
 </script>
 
 {#if data.status}
-	{#await api.getAthlete()}
+	<!-- {#await api.getAthlete()}
 		<SyncLoader />
 	{:then data}
 		<img src={data.profile} alt={'Athlete Profile Picture'} style="border-radius: 50%;" />
@@ -31,7 +32,10 @@
 		<Toast value={power} bins={bins}></Toast>
 	{:catch error}
 		<div>{error}</div>
-	{/await}
+	{/await} -->
+	<div class="w-screen h-screen flex">
+		<Toaster><Toast value={60} bins={[1, 2, 3, 4, 5, 6, 7, 60, 200, 300, 1000]} /></Toaster>
+	</div>
 {:else}
 	<button type="button" on:click={stravaOAuth}>Authenticate with Strava</button>
 {/if}

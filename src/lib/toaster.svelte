4aec466c7c78161c843dcae515ee05709ea59f40
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-02-12 17:45:10 +0000

    Poping Toast

--- src/lib/toaster.svelte
+++ src/lib/toaster.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
     // https://www.youtube.com/watch?v=G0HtHr46Awg&ab_channel=CodingArtist
 
-    export let status: "waiting" | "loading" |  "finished" = "waiting"
+    export let status: "waiting" | "loading" |  "finished" | "display"= "waiting"
 
 </script>
 <div>
     <button on:click={ ()=> status = "waiting"}>waiting</button>
     <button on:click={ ()=> status = "loading"}>loading</button>
     <button on:click={ ()=> status = "finished"}>finished</button>
+    <button on:click={ ()=> status = "display"}>display</button>
 </div>
 <div class="m-auto w-fit h-fit absolute inset-0">
-    <!-- <slot style ="width: 24rem; height 24rem;"/> -->
-    <div class="relative top-0 left- bg-blue-600 w-128 h-72 big-round"></div>
-    <div class="absolute top-0 right-0 bg-blue-400 w-96 h-72 big-round"></div>
-    <div class="absolute top-14 left-9 bg-gray-700 w-6 h-48 rounded-lg"></div>
-    <div class="absolute top-14 left-12 bg-gray-800 w-6 h-48 rounded-lg"></div>
-    <div class= {`absolute top-14 left-8 ${status} moving`}>
-        <div class="absolute bg-gray-300 w-14 h-6 rounded-md"></div>
-        <div class="relative bg-gray-100 w-12 h-6 rounded-md"></div>
+    <div class= {`absolute -top-10 left-44 w-56 h-56 toast-${status} moving`}>
+        <slot />
     </div>
-    <div class="absolute top-15 -left-4 bg-gray-700 w-132 h-6 rounded-lg"></div>
+    <div>
+        <div class="relative top-0 left- bg-blue-600 w-128 h-72 big-round"></div>
+        <div class="absolute top-0 right-0 bg-blue-400 w-96 h-72 big-round"></div>
+        <div class="absolute top-14 left-9 bg-gray-700 w-6 h-48 rounded-lg"></div>
+        <div class="absolute top-14 left-12 bg-gray-800 w-6 h-48 rounded-lg"></div>
+        <div class= {`absolute top-14 left-8 ${status} moving`}>
+            <div class="absolute bg-gray-300 w-14 h-6 rounded-md"></div>
+            <div class="relative bg-gray-100 w-12 h-6 rounded-md"></div>
+        </div>
+        <div class="absolute top-15 -left-4 bg-gray-700 w-132 h-6 rounded-lg"></div>
+    </div>
 </div>
 
 <style>
@@ -43,6 +48,30 @@
     .finished {
         transform: translate(0);
         transition: all 0.5s;
+    }
+    .display {
+        transform: translate(0);
+    }
+
+    .toast-waiting {
+        transform: translate(0, -100);
+    }
+    
+    .toast-loading {
+        transform: translate(0, 80px) ;
+    }
+
+    .toast-finished {
+        transform: translate(0, -300px);
+        transition: all 0.5s;
+    }
+
+    .toast-display {
+        transform: rotate(5deg) translate(-16rem, -100px);
+        transition: all 1s;
+        z-index: 1;
+        width: 40rem;
+        height: 40rem;
     }
 
 </style>

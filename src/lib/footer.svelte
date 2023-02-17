41d56e2d34d963f43304ce34388f661774a60ffa
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-02-17 18:29:39 +0000

    Added cursor style to social buttons

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -5,10 +5,10 @@
 
 <footer class="absolute w-content bottom-8 h-10 space-x-5 opacity-50 grid grid-flow-col ">
     <img class="w-32" src="images/api_logo_pwrdBy_strava_stack_gray.svg" alt="">
-    <a class ="grid content-center"  target="_blank" rel="noreferrer" href="https://twitter.com/canyoutoast">
+    <a class ="grid content-center cursor-pointer"  target="_blank" rel="noreferrer" href="https://twitter.com/canyoutoast">
         <SocialIcons network="twitter"/>
     </a>
-    <a class ="grid content-center" href="mailto:canyoutoast@gmail.com">
+    <a class ="grid content-center cursor-pointer" href="mailto:canyoutoast@gmail.com">
         <SocialIcons network="email"/>
     </a>
 </footer>

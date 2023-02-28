544276bc78acb5ff4ef144e76661e18fbaf21630
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-02-28 22:21:13 +0000

    Load 0 and lazyload rest limit display to one

--- src/lib/toast.svelte
+++ src/lib/toast.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { binLength, effortDetails, effortURLParams } from '$lib/types';
+	import { lazyLoad } from "$lib/lazyload"
 	import Share from './share.svelte';
 
 	export let effort: effortDetails;
@@ -9,27 +10,29 @@
 		src: string;
 		text: string;
 		altText: string;
+		idx: number;
 	}
 	// Places the value in one of 11 bins and decides which picture of toast to select based off that.
 	// First bin is <= value
 	// values exceeding the top bin value are handled
 	const toastSrc: toast[] = [
-		{ text: 'Thats just Bread', altText: 'Thats just Bread', src: '/images/toast/0.png' }, // 0 Bread
-		{ text: "I guess it's warm?", altText: 'Warmed Bread', src: '/images/toast/1.png' },
+		{ text: 'Thats just Bread', altText: 'Thats just Bread', src: '/images/toast/0.png' , idx: 0}, // 0 Bread
+		{ text: "I guess it's warm?", altText: 'Warmed Bread', src: '/images/toast/1.png', idx: 1 },
 		{
 			text: 'If you look closely',
 			altText: 'Slightly cooked if you look closely',
-			src: '/images/toast/2.png'
+			src: '/images/toast/2.png',
+			idx: 2
 		},
-		{ text: 'Just about!', altText: 'Just about toast', src: '/images/toast/3.png' }, // 3
-		{ text: 'Keep Trying', altText: 'almost toast keep trying', src: '/images/toast/4png' },
-		{ text: 'Not quite there', altText: 'Almost Done Toast', src: '/images/toast/5.png' },
-		{ text: 'Now thats Toast', altText: 'Builders Brew Toast', src: '/images/toast/6.png' }, // 6
-		{ text: 'A little over done', altText: 'a litte overdone Toast', src: '/images/toast/7.png' },
-		{ text: "That'll do", altText: 'overdone Toast', src: '/images/toast/8.png' },
-		{ text: 'Half way to dust', altText: 'half burnt', src: '/images/toast/9.png' },
-		{ text: 'Maybe edible', altText: 'almost burnt', src: '/images/toast/10.png' }, // 10
-		{ text: 'On Fire!', altText: 'Toast on Fire', src: '/images/toast/11.png' } // Burnt
+		{ text: 'Just about!', altText: 'Just about toast', src: '/images/toast/3.png', idx: 3 }, // 3
+		{ text: 'Keep Trying', altText: 'almost toast keep trying', src: '/images/toast/4png', idx: 4 },
+		{ text: 'Not quite there', altText: 'Almost Done Toast', src: '/images/toast/5.png', idx: 5 },
+		{ text: 'Now thats Toast', altText: 'Builders Brew Toast', src: '/images/toast/6.png', idx: 6 }, // 6
+		{ text: 'A little over done', altText: 'a litte overdone Toast', src: '/images/toast/7.png', idx: 7 },
+		{ text: "That'll do", altText: 'overdone Toast', src: '/images/toast/8.png', idx: 8 },
+		{ text: 'Half way to dust', altText: 'half burnt', src: '/images/toast/9.png', idx: 9 },
+		{ text: 'Maybe edible', altText: 'almost burnt', src: '/images/toast/10.png', idx: 10 }, // 10
+		{ text: 'On Fire!', altText: 'Toast on Fire', src: '/images/toast/11.png', idx: 11 } // Burnt
 	];
 
 	function binValue(value: number, bins: binLength) {
@@ -69,6 +72,7 @@
 	}
 
 	$: toast = toastSrc[binValue(effort.joules, bins)];
+	$: ToastIdx = binValue(effort.joules, bins);
 	$: {
 		toast
 		if (effort.id) {
@@ -131,7 +135,14 @@
 	<div style="font-size: 50px; bottom: 100%;" class="absolute text-center w-full">
 		{toast.text}
 	</div>
-	<img src={toast.src} alt={toast.altText} on:click={showShareButtons} on:keydown={showShareButtons}/>
+	{#if ToastIdx == 0}
+		<img src={toastSrc[0].src} alt={toastSrc[0].altText} on:click={showShareButtons} on:keydown={showShareButtons}/>
+	{/if}
+	{#each toastSrc.slice(1, -1) as toastOpt}
+		{#if ToastIdx == toastOpt.idx}
+			<img use:lazyLoad ={toastOpt.src} alt={toastOpt.altText} on:click={showShareButtons} on:keydown={showShareButtons}/>
+		{/if}
+	{/each}
 	<div style = {`top: ${sharePosY}px; left: ${sharePosX}px`} class="fixed -translate-y-1/2 -translate-x-1/2"><Share {showShare} {url}/></div>
 	
 	{#if effort.id != null}

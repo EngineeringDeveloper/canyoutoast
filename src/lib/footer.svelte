8e781d6654fd117984dc5324ebb22d01ee422c55
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-11-03 22:26:51 +0000

    added a whats this? link to the video source

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -3,14 +3,18 @@
 
 </script>
 
-<footer class="absolute w-content bottom-8 h-10 space-x-5 opacity-50 grid grid-flow-col ">
-    <img class="w-32" src="images/api_logo_pwrdBy_strava_stack_gray.svg" alt="">
-    <a class ="grid content-center cursor-pointer"  target="_blank" rel="noreferrer" href="https://twitter.com/canyoutoast">
-        <SocialIcons network="twitter"/>
-    </a>
-    <a class ="grid content-center cursor-pointer" href="mailto:canyoutoast@gmail.com">
-        <SocialIcons network="email"/>
-    </a>
+<footer class="absolute w-content bottom-10 h-10  opacity-50 grid grid-flow-row ">
+    <div class="grid grid-flow-col space-x-5 mx-auto">
+        <img class="w-32" src="images/api_logo_pwrdBy_strava_stack_gray.svg" alt="">
+        <a class ="grid content-center cursor-pointer"  target="_blank" rel="noreferrer" href="https://twitter.com/canyoutoast">
+            <SocialIcons network="twitter"/>
+        </a>
+        <a class ="grid content-center cursor-pointer" href="mailto:canyoutoast@gmail.com">
+            <SocialIcons network="email"/>
+        </a>
+    </div>
+    <h2>What is this?</h2>
+    <iframe class="w-full xl:w-142 xl:h-96"  src="https://www.youtube.com/embed/S4O5voOCqAQ?si=JxFaAHl33I1ZAMsc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </footer>
 
 <style>

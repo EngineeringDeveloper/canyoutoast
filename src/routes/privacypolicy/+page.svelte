8c45d65179c2efbbf4a5029e04036ea7450a2b38
EngineeringDeveloper <ryan.c.parki@gmail.com>
2025-01-03 17:38:45 +0000

    added privacy policy page

---
+++ src/routes/privacypolicy/+page.svelte
@@ -0,0 +1,33 @@
+<div class= "container mx-auto p-4">
+    <h1 class=" text-3xl font-bold">
+    Privacy Policy for the CanYouToast? Chrome Extension
+    </h1>
+    <br>
+    <div>Effective Date: 1st January 2025</div>
+    <br>
+    <div>The CanYouToast? development team, is committed to maintaining the privacy of our users. This Privacy Policy describes how the CanYouToast? Chrome Extension ("the Extension") handles user data.</div>
+    <br>
+    <h2 class="text-2xl">
+        Data Collection and Use
+    </h2>
+    <br>
+    <div>The CanYouToast? Extension is designed to operate entirely client-side. This means that all processing and functionality occur within your web browser on your local device. The Extension does not collect, transmit, store, or share any personal information, browsing history, or any other user data. No data is sent to our servers or any third-party servers.</div>
+    <br>
+    <h2 class="text-2xl">
+        Information Sharing
+    </h2>
+    <br>
+    <div>Because no user data is collected, there is no information to share with third parties. We do not sell, rent, or otherwise disclose any user information.</div>
+    <br>
+    <h2 class=" text-2xl">
+        Changes to this Privacy Policy
+    </h2>
+    <br>
+    <div>We may update this Privacy Policy from time to time. Any changes will be posted on this page with a revised effective date. We encourage you to review this Privacy Policy periodically for any updates.</div>
+    <br>
+    <h2 class="text-2xl">
+        Contact Us
+    </h2>
+    <br>
+    <div>If you have any questions or concerns regarding this Privacy Policy, please contact us at: canyoutoast@gmail.com</div>
+</div>
